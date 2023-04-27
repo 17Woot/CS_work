24d4ae04cd15bbf45e9de1fbaed895eac0d60c02
17Woot <113183909+17Woot@users.noreply.github.com>
2023-04-27 22:47:54 +0100

    Add files via upload

---
+++ project_clss.py
@@ -0,0 +1,28 @@
+#import pygame
+
+
+#class Planet(object):
+   # AU = 149.6e6 * 1000 # 1 AU in meters
+    #G = 6.67428e-11 # Gravitational constant
+    #SCALE = 250 / AU # Scale to fit all planets on the screen , 1AU = 100 PIXELS
+    #TIMESTEP = 24 * 3600 # Simulation time step (1 day), update the position of the planet by 1 day
+
+    #def __init__(self, x ,y, radius, color, mass):
+     #   self.x = x
+      #  self.y = y
+     #   self.radius = radius
+      #  self.color = color
+       # self.mass = mass
+#
+       # self.orbit = []
+       # self.sun = False
+       # self.distance_to_sun = 0
+
+       # self.x_vel = 0
+        #self.y_vel = 0
+    
+   # def draw(self, win):
+      #  x = self.x * self.SCALE + WIDTH / 2
+       # y = self.y * self.SCALE + HEIGHT / 2
+       # pygame.draw.circle(win, self.color, (int(x), int(y)), self.radius)
+
